7b4560dca4aeb8956567d9b87f56e6c212661a7d
oponei <56514281+OJoaoSP@users.noreply.github.com>
2024-08-23 09:00:42 -0300

    Ajuste em regra de adição de peça ao tabuleiro

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # lig4
+Projeto desenvolvido para fins de estudo, recriando o jogo lig4. <br>
+
+## Regras do jogo
+Quantidade de players: 1 ou 2 (1 podendo ser robo) <br>
+Cada jogador pode selecionar 1 coluna para jogar sua peça e cairá até o fundo do tabuleiro. <br>
+Ganha o jogador que tiver 4 peças em sequencia horizontal, vertical ou diagonal.
--- lig4/src/Table.java
+++ lig4/src/Table.java
@@ -4,7 +4,6 @@ import java.util.List;
 public class Table {
   private List<List<String>> matrix;
 
-
   public Table(){
     matrix = new ArrayList<List<String>>();
     this.matrix.add(new ArrayList<String>() );
@@ -15,17 +14,25 @@ public class Table {
     this.matrix.add(new ArrayList<String>() );
 
     for(List<String> row_m: this.matrix){
-      for ( int i = 0; i <6; i++){ row_m.add("_") ;}
+      for ( int i = 0; i <7; i++){ row_m.add("_") ;}
 
     }
   }
 
 
-  public void play(Integer column, String tipo){
+  public void play(Integer column, PlayerEntity player){
+    String piece = player.gamePiece();
+    // precisa colocar uma validação se ainda tem como colocar nessa coluna.
     List<String> befor_row = new ArrayList<>();
+    var last_row = matrix.getLast();
     for (List<String> row: matrix){
+      if (row == last_row && row.get(column).equals("_") ){
+        row.set(column,piece );
+        return;
+      }
       if (!row.get(column).equals("_")){
-        befor_row.set(column, tipo); // <- tem alguma coisa que talvez esteja errada aqui
+        befor_row.set(column, piece);
+        return;
       }
       befor_row = row;
     }
@@ -42,6 +49,7 @@ public class Table {
       System.out.println(rowString);
 
     }
+    System.out.println("=".repeat(40));
     return "";
   }
 }
